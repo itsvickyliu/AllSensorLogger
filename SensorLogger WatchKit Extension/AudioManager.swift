47a26aac9f763b25407661628a8426b2cbd15f69
Vicky Liu <vickylry@vip.sina.com>
2022-06-23 14:15:38 -0400

    Enable real-time audio transfer in AAC format

--- SensorLogger WatchKit Extension/AudioManager.swift
+++ SensorLogger WatchKit Extension/AudioManager.swift
@@ -8,218 +8,134 @@
 import WatchKit
 import AVFoundation
 import WatchConnectivity
-//import CloudKit
-//
-//class AudioManager:  WKInterfaceController, AVAudioRecorderDelegate{
-//    var recordingSession: AVAudioSession!
-//    var recorder: AVAudioRecorder!
-//    var audioURL = FileManager.default.getDocumentsDirectory()
-//    var timeURL = FileManager.default.getDocumentsDirectory()
-//    var audioPlayer: AVAudioPlayer!
-//    var timeText = ""
-//
-//    func startRecording(participantID pID: String, sessionID sID: String) {
-//        audioURL = audioURL.appendingPathComponent("\(pID)-\(sID).wav")
-//        timeURL = timeURL.appendingPathComponent("\(pID)-\(sID)-audiotime.txt")
-//
-//        let settings = [
-//          AVFormatIDKey: Int(kAudioFormatLinearPCM),
-//          AVSampleRateKey: 32000,
-//          AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-//        ]
-//
-//        do {
-//            recorder = try AVAudioRecorder(url: audioURL, settings: settings)
-//            recorder.delegate = self
-//            recorder.record()
-//            timeText += "Start time: \(NSDate().timeIntervalSince1970) \n"
-//            print ("Start recording")
-//        } catch {
-//            print ("Recording failed")
-//        }
-//    }
-//
-//    func endRecording() {
-//        recorder.stop()
-//        timeText += "End time: \(NSDate().timeIntervalSince1970)"
-//        print ("End recording")
-//        do {
-//            try timeText.write(to: timeURL, atomically: true, encoding: .utf8)
-//        } catch {
-//            print("Error", error)
-//            return
-//        }
-//        recorder = nil
-//    }
-//
-//    func setupView() {
-//            recordingSession = AVAudioSession.sharedInstance()
-//
-//            do {
-//                try recordingSession.setCategory(.playAndRecord, mode: .default)
-//                try recordingSession.setActive(true)
-//                recordingSession.requestRecordPermission() { [unowned self] allowed in
-//                    DispatchQueue.main.async {
-//                        if allowed {
-//                        } else {
-//                            print("Failed to record")
-//                        }
-//                    }
-//                }
-//            } catch {
-//                print("Failed to record")
-//            }
-//        }
-//
-//}
-//
-//class AudioManager {
-//    enum RecordingState {
-//        case recording, stopped
-//    }
-//
-//    var engine: AVAudioEngine!
-//    var mixerNode: AVAudioMixerNode!
-//    var state: RecordingState = .stopped
-//    var audioURL = FileManager.default.getDocumentsDirectory()
-//    var timeURL = FileManager.default.getDocumentsDirectory()
-//
-//    init() {
-//        setupSession()
-//        setupEngine()
-//    }
-//
-//    func setupSession() {
-//      let session = AVAudioSession.sharedInstance()
-//      try? session.setCategory(.record)
-//      try? session.setActive(true, options: .notifyOthersOnDeactivation)
-//    }
-//
-//    func setupEngine() {
-//        engine = AVAudioEngine()
-//        mixerNode = AVAudioMixerNode()
-//
-//        mixerNode.volume = 0
-//        engine.attach(mixerNode)
-//
-//        makeConnections()
-//
-//        engine.prepare()
-//    }
-//
-//    func makeConnections() {
-//        let inputNode = engine.inputNode
-//        let inputFormat = inputNode.outputFormat(forBus: 0)
-//        engine.connect(inputNode, to: mixerNode, format: inputFormat)
-//        let mainMixerNode = engine.mainMixerNode
-//        let mixerFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: inputFormat.sampleRate, channels: 1, interleaved: false)
-//        engine.connect(mixerNode, to: mainMixerNode, format: mixerFormat)
-//    }
-//
-//    func startRecording(participantID pID: String, sessionID sID: String) throws {
-//        let tapNode: AVAudioNode = mixerNode
-//        let format = tapNode.outputFormat(forBus: 0)
-//        audioURL = audioURL.appendingPathComponent("\(pID)-\(sID).caf")
-//        timeURL = timeURL.appendingPathComponent("\(pID)-\(sID)-audiotime.txt")
-//
-//        //let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-//
-//        // AVAudioFile uses the Core Audio Format (CAF) to write to disk.
-//        // So we're using the caf file extension.
-//        let file = try AVAudioFile(forWriting: audioURL, settings: format.settings)
-//
-//        tapNode.installTap(onBus: 0, bufferSize: 4096, format: format, block: {
-//        (buffer, time) in
-//            try? file.write(from: buffer)
-//        })
-//
-//      try engine.start()
-//      state = .recording
-//    }
-//
-//    func endRecording() {
-//      // Remove existing taps on nodes
-//      mixerNode.removeTap(onBus: 0)
-//
-//      engine.stop()
-//      state = .stopped
-//    }
-//}
 
 class AudioManager {
+    
+    // AVAudioEngine used to record
+    var engine = AVAudioEngine()
 
-    let engine = AVAudioEngine()
+    // Set this as per your liking (512, 1024, 2048)
+    let estimatedBufferSize: AVAudioFrameCount = 1024
+
+    // Will be configured to write the buffer to a file
+    var file: AVAudioFile?
+
+    // Chunk duration in seconds, adjust as needed
+    let chunkDuration: Float64 = 3
+
+    // Will be used to uniquely name the different chunks
+    var currentChunkCount = 0
+
+    // Keeps track of how many frames in current chunk
+    // Used to check how much time has elapsed
+    var framesInCurrentChunk: AVAudioFrameCount = 0
+    
     var audioURL = FileManager.default.getDocumentsDirectory()
-
-    struct K {
-        static let secondsPerChunk: Float64 = 10
-    }
-
-    var chunkFile: AVAudioFile! = nil
-    var outputFramesPerSecond: Float64 = 0  // aka input sample rate
-    var chunkFrames: AVAudioFrameCount = 0
-    var chunkFileNumber: Int = 0
-
-    func writeBuffer(_ buffer: AVAudioPCMBuffer, participantID pID: String, sessionID sID: String) {
-        let samplesPerSecond = buffer.format.sampleRate
-
-        if chunkFile == nil {
-            createNewChunkFile(numChannels: buffer.format.channelCount, samplesPerSecond: samplesPerSecond, participantID: pID, sessionID: sID)
-        }
-
-        try! chunkFile.write(from: buffer)
-        chunkFrames += buffer.frameLength
-
-        if chunkFrames > AVAudioFrameCount(K.secondsPerChunk * samplesPerSecond) {
-            chunkFile = nil // close file
-        }
-    }
-
-    func createNewChunkFile(numChannels: AVAudioChannelCount, samplesPerSecond: Float64, participantID pID: String, sessionID sID: String) {
-//        audioURL = audioURL.appendingPathComponent("\(pID)-\(sID)-\(chunkFileNumber).aac")
-//        audioURL = audioURL.appendingPathComponent("\(pID)-\(sID)")
-        let audioURL = FileManager.default.getDocumentsDirectory().appendingPathComponent("\(pID)-\(sID)-\(chunkFileNumber).aac")
-        print("writing chunk to \(audioURL)")
-
-        let settings: [String: Any] = [
-            AVFormatIDKey: kAudioFormatMPEG4AAC,
-            AVEncoderBitRateKey: 64000,
-            AVNumberOfChannelsKey: numChannels,
-            AVSampleRateKey: samplesPerSecond
-        ]
-
-        chunkFile = try! AVAudioFile(forWriting: audioURL, settings: settings)
-        
-        if (WCSession.default.isReachable) {
-            WCSession.default.transferFile(audioURL, metadata: nil)
-            print ("Audio data transferred")
-        } else {
-            print ("WCSession is not activated")
-        }
-
-        chunkFileNumber += 1
-        chunkFrames = 0
-    }
     
     func startRecording(participantID pID: String, sessionID sID: String) {
-        let input = engine.inputNode
-
-        let bus = 0
-        let inputFormat = input.inputFormat(forBus: bus)
         
-        input.removeTap(onBus: bus)
-        input.installTap(onBus: bus, bufferSize: 512, format: inputFormat) { (buffer, time) -> Void in
-            DispatchQueue.main.async {
-                self.writeBuffer(buffer, participantID: pID, sessionID: sID)
-            }
+        print("Start audio updates")
+            
+        // Prepare how the AVAudioEngine should process input
+        engine.inputNode.installTap(onBus: 0,
+                                    bufferSize: 1024,
+                                    format: engine.inputNode.inputFormat(forBus: 0))
+        { [weak self] (buffer, time) -> Void in
+                
+            // Write the buffer to your file
+            self?.writeBufferToFile(buffer: buffer, participantID: pID, sessionID: sID)
         }
-
+            
+        // Start recording
         try! engine.start()
     }
     
-    func endRecording() {
-        engine.stop()
+    func endRecording(participantID pID: String, sessionID sID: String) {
         
+        print("End audio updates")
+            
+        // Clean up and reset
+        engine.inputNode.removeTap(onBus: 0)
+        engine.stop()
+        file = nil
+        framesInCurrentChunk = 0
+            
+        // Here is where you should upload any files that have not been uploaded
+        // Delete files after upload if you want or manage file name duplicates
+        if (WCSession.default.isReachable) {
+            audioURL = FileManager.default.getDocumentsDirectory().appendingPathComponent("\(pID)-\(sID)-\(currentChunkCount).aac")
+            WCSession.default.transferFile(audioURL, metadata: nil)
+        } else {
+            print ("WCSession is not activated")
+        }
+    
+    }
+    
+    private func writeBufferToFile(buffer: AVAudioPCMBuffer, participantID pID: String, sessionID sID: String)
+    {
+        let samplesPerSecond = buffer.format.sampleRate
+        
+        // Check if we have an open file writer
+        if file == nil
+        {
+            // Configure an AVAudioFile to write the audio buffer to file
+            prepareOutputFile(participantID: pID, sessionID: sID)
+        }
+        
+        do
+        {
+            try file?.write(from: buffer)
+            framesInCurrentChunk += buffer.frameLength
+        }
+        catch
+        {
+            // error appending the chunk to file
+            print(error)
+        }
+        
+        
+        // Check if the current chunk has reached it's duration
+        if framesInCurrentChunk > AVAudioFrameCount(chunkDuration * samplesPerSecond)
+        {
+            // Here is where you have a valid chunk that has been saved in
+            // the duration you want, put the last saved aac file in a queue to be
+            // uploaded to your server here
+            
+            // De-initialize the current file writer so we can start a new one
+            file = nil
+        }
     }
 
+    private func prepareOutputFile(participantID pID: String, sessionID sID: String)
+    {
+        // Increment the current chunk count to create a new file
+        currentChunkCount += 1
+        
+        if (WCSession.default.isReachable && currentChunkCount > 1) {
+            audioURL = FileManager.default.getDocumentsDirectory().appendingPathComponent("\(pID)-\(sID)-\(currentChunkCount-1).aac")
+            WCSession.default.transferFile(audioURL, metadata: nil)
+        } else {
+            print ("WCSession is not activated")
+        }
+        
+        audioURL = FileManager.default.getDocumentsDirectory().appendingPathComponent("\(pID)-\(sID)-\(currentChunkCount).aac")
+        
+        print("Recording audio to path: \(audioURL)")
+        
+        do
+        {
+            // Configure the AVAudioFile with the output path and output format
+            file = try AVAudioFile(forWriting: audioURL,
+                                   settings: [AVFormatIDKey: kAudioFormatMPEG4AAC])
+            
+        }
+        catch
+        {
+            // Handle errors in configuring the the AVAudioFile
+            print(error)
+        }
+        
+        // Reset the frames saved in the current chunk
+        framesInCurrentChunk = 0
+    }
 }
--- SensorLogger/ViewController.swift
+++ SensorLogger/ViewController.swift
@@ -103,15 +103,15 @@ class ViewController: UIViewController {
                     encoding: .utf8
                 )
             DispatchQueue.main.async {
-                self.watchStatusLabel.text = "Saved motion data"
+                self.watchStatusLabel.text = "Saved motion and audio data [STUDY END]"
                 self.pairButton.isEnabled = false
                 self.startButton.isEnabled = false
                 self.stopButton.isEnabled = false
             }
         } catch {
-            print("Error", error)
+            print(error)
             DispatchQueue.main.async {
-                self.watchStatusLabel.text = "Fail to save motion data"
+                self.watchStatusLabel.text = "Failed to save motion and audio data"
             }
         }
     }
@@ -161,22 +161,11 @@ extension ViewController: WCSessionDelegate {
     
     func session(_ session: WCSession, didReceive file: WCSessionFile) {
         count += 1
-        
-        var dataURL = FileManager.default.getDocumentsDirectory()
-//        if (file.fileURL.absoluteString.contains(".txt")) {
-//            dataURL = dataURL.appendingPathComponent("\(participantText)-\(sessionText)-audiotime.txt")
-//        } else if (file.fileURL.absoluteString.contains(".caf")) {
-//            dataURL = dataURL.appendingPathComponent("\(participantText)-\(sessionText).caf")
-//        }
-        if (file.fileURL.absoluteString.contains(".aac")) {
-            dataURL = dataURL.appendingPathComponent("\(participantText)-\(sessionText)-\(count).aac")
-        }
+        let dataURL = FileManager.default.getDocumentsDirectory().appendingPathComponent("\(participantText)-\(sessionText)-\(count).aac")
+
         do {
             try FileManager.default.moveItem(at: file.fileURL, to: dataURL)
-            // logging to know if the audio data is saved
-            DispatchQueue.main.async {
-                self.watchStatusLabel.text = "Saved Audio Files [STUDY END]"
-            }
+            
         }
         catch let error{
             print (error)
@@ -185,7 +174,6 @@ extension ViewController: WCSessionDelegate {
     
     func session(_ session: WCSession, didReceiveMessage message: [String : Any] = [:]) {
         if let motionData = message["motionData"] as? String {
-            print ("Received motion data")
             savedText += motionData
             
             // real-time logging on the phone
